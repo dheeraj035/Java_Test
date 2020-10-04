0ca4ccb12f0d6a844193d722efa425a430f8f60c
Dheeraj <dheeraj.s@ikyam.com>
2020-10-04 15:15:26 +0000

    Java file for jenkins job

---
+++ Hello.java
@@ -0,0 +1,9 @@
+public class Hello
+
+{
+	public static void main (string[] args)
+	{
+		System.out.println("Welcome to jenkins building java based file");
+	}
+}
+
--- sample.py
+++
@@ -1,6 +0,0 @@
-a=10
-b=20
-c=30
-
-print(c)
-print(a+b)
